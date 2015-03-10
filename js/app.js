0f4308a04e5339abed04a54b34457584faf56571
AlexSYanai <yanai.alex@gmail.com>
2015-03-10 03:38:39 -0400

    adds controller for save function

--- js/app.js
+++ js/app.js
@@ -12,3 +12,31 @@ Ember.Handlebars.helper('format-markdown', function(input) {
     return "";
     return new Handlebars.SafeString(showdown.makeHtml(input));
   });
+
+App.Note = DS.Model.extend({
+  copy: DS.attr()
+});
+
+App.NotesRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find("note");
+  }
+});
+
+App.NotesController = Ember.ArrayController.extend({
+  actions: {
+    markdownInput: function() {
+      var copy = this.get("markdownInput");
+      if (!copy) {
+        return false;
+      }
+
+      var note = this.store.createRecord("note", {
+        copy: copy
+      });
+
+      this.set("markdownInput", "");
+      note.save();
+    }
+  }
+});
